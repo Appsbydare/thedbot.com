fb5c930165d47d6dcd56c4f22a1424899abc0bd4
Appsbydare <appsbydarshana@gmail.com>
2025-10-24 21:15:07 +0530

    Add X (close) icon to fix PaymentModal build error

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -309,6 +309,25 @@ export function CheckCircle(props: IconProps) {
   );
 }
 
+export function X(props: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={props.className}
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+    >
+      <line x1="18" y1="6" x2="6" y2="18" />
+      <line x1="6" y1="6" x2="18" y2="18" />
+    </svg>
+  );
+}
+
 const Icons = {
   ArrowRight,
   ArrowLeft,
@@ -326,6 +345,7 @@ const Icons = {
   Sun,
   Moon,
   CheckCircle,
+  X,
 };
 
 export default Icons;
